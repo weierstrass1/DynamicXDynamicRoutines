2a7610a741290743d510a99496ce9a3984e7f6cc
Sneed <127693805+TrashRaccoonSnuffy@users.noreply.github.com>
2023-09-02 22:21:42 -0400

    Update DynamicPoseHashmapSlotTests.asm

--- ASM/DynamicXRoutinesTestROM/main/tests/DynamicPoseHashmapSlotTests.asm
+++ ASM/DynamicXRoutinesTestROM/main/tests/DynamicPoseHashmapSlotTests.asm
@@ -1,5 +1,6 @@
 ;determine with BEQ to see if test passed
 DynamicPoseHashmapSlotTests_TestGetHashCode:
+    STZ.B TEST_STATUS
     LDA.B #$85
 	AND.b #!HASHMAP_SIZE-1 ;DynamicPoseHashMapSlot.GetHashCode()
     CMP.B #$05
@@ -9,8 +10,8 @@ DynamicPoseHashmapSlotTests_TestGetHashCode:
 RTL
 
 SlotTestsStrings:
-    dl SlotTestsRetP
     dl SlotTestsRetF
+    dl SlotTestsRetP
 
 SlotTestsRetP: db "Test SlotTests: Pasado",$00
 SlotTestsRetF: db "Test SlotTests: Fallado",$00
